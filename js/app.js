f88dbdae80b5a6881bcb7734d0f25f0ffeb7b97a
Mehdi Fyber <mahdisadeghi1381m@gmail.com>
2021-08-26 00:23:56 +0430

    Main event Listener function added!
    and form selected and prevented Default

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 // To do App js file!!
+
+// variabls
+
+
+// eventlisteners
+// general event listeners function!
+eventListeners()
+function eventListeners() {
+    document.querySelector('#form').addEventListener('submit',newNote)
+}
+
+
+
+// functions
+// form newNote function
+function newNote(event) {
+    event.preventDefault();
+}
