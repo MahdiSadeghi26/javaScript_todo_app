666af2b5eb9dc442780c3cbb6ffe49bb4deb2c2f
Mehdi Fyber <mahdisadeghi1381m@gmail.com>
2021-08-27 18:36:23 +0430

    validator of textContent of li added!

--- js/app.js
+++ js/app.js
@@ -14,20 +14,19 @@ function eventListeners() {
 }
 
 // functions
-
 // Adding new note to list  (form function)
 function newNote(event) {
   event.preventDefault();
   // textarea value access
   const note = document.querySelector("#note").value;
 
+  // li variable (<li> Tag)
+  const li = document.createElement("li");
+
   // create remove button
   const removeBtn = document.createElement("a");
   removeBtn.textContent = "X";
   removeBtn.classList = "remove-btn";
-
-  // li variable (<li> Tag)
-  const li = document.createElement("li");
 
   // adding remove btn to li
   li.appendChild(removeBtn);
@@ -35,4 +34,8 @@ function newNote(event) {
   // adding note text to li
   li.appendChild(document.createTextNode(note));
   noteItems.appendChild(li);
+  // validating textnote length
+  if (li.textContent.length===1) {
+    li.remove()
+  }
 }
